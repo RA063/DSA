dac9de7cee08638d085d38eb5955555636502c56
Rimpa Adhikary <141876300+Rimpa-Adhikary@users.noreply.github.com>
2024-09-25 23:59:22 +0530

    Add files via upload

---
+++ InfixToPostfix.cpp
@@ -0,0 +1,58 @@
+# include <iostream>
+#include <stack>
+using namespace std;
+bool isopr(char c)
+{
+	return (c=='+'||c=='-'||c=='*'||c=='/'||c=='^');
+}
+int prec(char c){
+	if(c=='+'||c=='-')
+		return 1;
+	else if(c=='*'||c=='/')
+		return 2;
+	else if(c=='^')
+		return 3;
+	else
+		return -1;
+}
+string infix_to_postfix(string s)
+{
+	stack<char> st;
+	string res;
+	for(int i=0; i< s.length();i++)
+	{
+		if(s[i]>='a'&&s[i]<='z' || s[i]>='A'&&s[i]<='Z' ){
+			res+=s[i];	
+		}
+		else if(s[i]=='('){
+		st.push(s[i]);
+		}
+		else if(s[i]==')')
+		{
+			while(!st.empty() && st.top() !='(')
+			{
+			res+=st.top();
+			st.pop();
+			}
+			if(!st.empty()){
+			st.pop();	}
+		}
+		else{
+		if(!st.empty() && prec(st.top())>prec(s[i])){
+			res+=st.top();
+			st.pop();
+		}
+		st.push(s[i]);
+		}
+	}
+	while(!st.empty()){
+		res+=st.top();
+		st.pop();
+	}
+	return res;
+}
+//main
+int main(){
+cout <<infix_to_postfix("(a-b/c)*(a/k-l)");
+return 0;
+}
